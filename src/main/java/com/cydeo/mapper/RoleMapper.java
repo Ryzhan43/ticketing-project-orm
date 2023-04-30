bae2d010991e4498258c828e43a81b96cfd5f279
ryzhan43 <ryzhan43@gmail.com>
2023-04-30 13:40:37 -0400

    ModelMapper added

---
+++ src/main/java/com/cydeo/mapper/RoleMapper.java
@@ -0,0 +1,27 @@
+package com.cydeo.mapper;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.entity.Role;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleMapper {
+
+    private final ModelMapper modelMapper;
+
+    public RoleMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    //convertToEntity
+    public Role convertToEntity(RoleDTO dto){
+        return modelMapper.map(dto, Role.class);
+    }
+
+
+    //convertToDto
+    public RoleDTO convertToDTO(Role role){
+        return modelMapper.map(role, RoleDTO.class);
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles(description) values ('Admin');
+INSERT INTO roles(description) values ('Manager');
+INSERT INTO roles(description) values ('Employee');
