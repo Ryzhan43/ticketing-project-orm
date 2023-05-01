37f5fd98ba3f5a747a02973499b0922128183e01
ryzhan43 <ryzhan43@gmail.com>
2023-04-30 21:03:56 -0400

    ModelMapper added

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -9,5 +9,5 @@ import java.util.List;
 public interface RoleService {
 
     List<RoleDTO> findALlRoles();
-    RoleDTO findById(String id);
+    RoleDTO findById(Long id);
 }
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -26,7 +26,7 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public RoleDTO findById(String id) {
-        return null;
+    public RoleDTO findById(Long id) {
+        return roleMapper.convertToDTO(roleRepository.findById(id).get());
     }
 }
