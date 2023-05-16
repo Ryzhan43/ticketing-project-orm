62664559469a6db743da3f344553f97f677a37bf
ryzhan43 <ryzhan43@gmail.com>
2023-05-16 18:48:51 -0400

    Security adding

--- src/main/java/com/mryzhan/config/SecurityConfig.java
+++ src/main/java/com/mryzhan/config/SecurityConfig.java
@@ -23,19 +23,22 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder){
 
-        List<UserDetails> userList = new ArrayList<>();
-
-        userList.add(
-          new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-          new User("danny", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")));
+        List<UserDetails> userList = Arrays.asList(new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))),
+                                                    new User("danny", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")))
+        );
 
           return new InMemoryUserDetailsManager(userList);
     }
 
 
-    public SecurityFilterChain filterChain(HttpSecurity http){
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests()
+                .antMatchers("user/**").hasRole("ADMIN")
+                .antMatchers("project/**").hasRole("MANAGER")
+                .antMatchers("task/employee/**").hasRole("EMPLOYEE")
+//                .antMatchers("task/**").hasAnyRole("EMPLOYEE", "ADMIN")
+//                .antMatchers("task/**").hasAuthority("ROLE_EMPLOYEE")
                 .antMatchers(
                         "/",
                      "/login",
@@ -45,7 +48,12 @@ public class SecurityConfig {
                 ).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic()
+//                .httpBasic()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/welcome")
+                .failureUrl("/login/error=true")
+                .permitAll()
                 .and().build();
     }
 
